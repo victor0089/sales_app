5ab89dfdf8d8d806cac4f1460ab88ad9d0fab6a4
Victor <victor.mecdoors@gmail.com>
2024-01-13 21:00:44 +0200

    update

--- sales/urls.py
+++ sales/urls.py
@@ -1,7 +1,9 @@
 # sales/urls.py
 
 from django.urls import path
-from .views import add_sale, add_product, add_customer, add_branch, add_accounting
+from .views import add_sale, add_product, add_customer, add_branch, add_accounting, add_expense, expenses_list
+from .views import add_general_journal, general_journals_list, add_ledger, ledgers_list, add_trial_balance, trial_balances_list
+from .views import add_income_summary, income_summaries_list
 
 urlpatterns = [
     path('add-sale/', add_sale, name='add_sale'),
@@ -9,6 +11,16 @@ urlpatterns = [
     path('add-customer/', add_customer, name='add_customer'),
     path('add-branch/', add_branch, name='add_branch'),
     path('add-accounting/', add_accounting, name='add_accounting'),
+    path('add-expense/', add_expense, name='add_expense'),
+    path('expenses-list/', expenses_list, name='expenses_list'),
+    path('add-general-journal/', add_general_journal, name='add_general_journal'),
+    path('general-journals-list/', general_journals_list, name='general_journals_list'),
+    path('add-ledger/', add_ledger, name='add_ledger'),
+    path('ledgers-list/', ledgers_list, name='ledgers_list'),
+    path('add-trial-balance/', add_trial_balance, name='add_trial_balance'),
+    path('trial-balances-list/', trial_balances_list, name='trial_balances_list'),
+    path('add-income-summary/', add_income_summary, name='add_income_summary'),
+    path('income-summaries-list/', income_summaries_list, name='income_summaries_list'),
     # Add URLs for other views
 ]
 
--- sales/views.py
+++ sales/views.py
@@ -1,8 +1,27 @@
 # sales/views.py
 
 from django.shortcuts import render, redirect
-from .models import Product, Customer, Sale, Inventory, Branch, Accounting
-from .forms import SaleForm, ProductForm, CustomerForm
+from .models import Product, Customer, Sale, Inventory, Branch, Accounting, Expense, GeneralJournal, JournalEntry
+from .models import Ledger, TrialBalance, IncomeSummary, Deposit, Withdraw, CashDiscount, SalesReturnsAllowances
+from .models import Purchases, PurchasesReturnsAllowances, NotesReceivable, AccountsReceivable, InventoryAdjustingEntry, AdjustingEntry
+from .forms import SaleForm, ProductForm, CustomerForm, ExpenseForm, GeneralJournalForm, JournalEntryForm
+from .forms import LedgerForm, TrialBalanceForm, IncomeSummaryForm, DepositForm, WithdrawForm, CashDiscountForm, SalesReturnsAllowancesForm
+from .forms import PurchasesForm, PurchasesReturnsAllowancesForm, NotesReceivableForm, AccountsReceivableForm, InventoryAdjustingEntryForm, AdjustingEntryForm
+
+def add_income_summary(request):
+    if request.method == 'POST':
+        form = IncomeSummaryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('income_summaries_list')
+    else:
+        form = IncomeSummaryForm()
+
+    return render(request, 'sales/add_income_summary.html', {'form': form})
+
+def income_summaries_list(request):
+    summaries = IncomeSummary.objects.all()
+    return render(request, 'sales/income_summaries_list.html', {'summaries': summaries})
 
 def add_branch(request):
     if request.method == 'POST':
@@ -21,4 +40,34 @@ def add_accounting(request):
     return render(request, 'sales/add_accounting.html')
 
 # Implement views for other modules similarly
+def add_ledger(request):
+    if request.method == 'POST':
+        form = LedgerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('ledgers_list')
+    else:
+        form = LedgerForm()
 
+    return render(request, 'sales/add_ledger.html', {'form': form})
+
+def ledgers_list(request):
+    ledgers = Ledger.objects.all()
+    return render(request, 'sales/ledgers_list.html', {'ledgers': ledgers})
+
+def add_trial_balance(request):
+    if request.method == 'POST':
+        form = TrialBalanceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('trial_balances_list')
+    else:
+        form = TrialBalanceForm()
+
+    return render(request, 'sales/add_trial_balance.html', {'form': form})
+
+def trial_balances_list(request):
+    trial_balances = TrialBalance.objects.all()
+    return render(request, 'sales/trial_balances_list.html', {'trial_balances': trial_balances})
+
+# Implement views for other financial statements similarly
