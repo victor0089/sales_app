1a3987c8c1d5d96fe51ba6e9abdeb355a9be1b0a
Victor Adly <42696918+victor0089@users.noreply.github.com>
2024-01-13 23:19:49 +0200

    Update urls.py

--- sales_app/urls.py
+++ sales_app/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import add_sale
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('add-sale/', add_sale.urls)
 ]
